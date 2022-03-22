5cdaed86e940bfe4bc3ca13b3d1f2352ac91f24a
GlennPS <glennsculthorp@gmail.com>
2022-03-22 15:07:39 +0000

    navbar initial try

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,1 +1,73 @@
 // nav bar
+
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom"
+
+function NavBar() {
+    const [click, setClick] = useState(false);
+  
+    const handleClick = () => setClick(!click);
+    return (
+      <>
+        <nav className="navbar">
+          <div className="nav-container">
+            <NavLink exact to="/" className="nav-logo">
+              Viewfinder
+            </NavLink>
+  
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/about"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Favourites
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/blog"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Settings
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/contact"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={handleClick}
+                >
+                  Log Out
+                </NavLink>
+              </li>
+            </ul>
+            <div className="nav-icon" onClick={handleClick}>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  }
+  
+  export default NavBar;
+  
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,1 +1,11 @@
 // home 
+
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
