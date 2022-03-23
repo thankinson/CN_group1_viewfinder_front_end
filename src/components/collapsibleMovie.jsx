2d55ac260bd972898bdf78408ebdbb40a122807c
Sha <charlotte.megroff@outlook.com>
2022-03-23 11:32:59 +0000

    Star ratings

--- src/components/collapsibleMovie.jsx
+++ src/components/collapsibleMovie.jsx
@@ -14,6 +14,21 @@ import TriangleFill from  "../assets/triangle-fill1.svg";
 
 const { REACT_APP_API_KEY } = process.env;
 //
+
+const StarRating = ({ stars }) =>  {
+    let fiveStars = [0,0,0,0,0];
+    fiveStars = fiveStars.slice(0, Math.round(stars));
+    return (
+            <StarDiv>
+                { fiveStars && fiveStars.map(
+                        (item, index) => <SmallLogo src={Star} key= {`Star${index}`}/>
+                    )
+                }
+            </StarDiv>
+    ) 
+
+}
+
 export const CollapsibleMovie = ( { moviesArray } ) => {
     const [watchlist, setWatchlist] = useState([]);
     const [movieSearch, setMovieSearch] = useState();
@@ -50,7 +65,10 @@ export const CollapsibleMovie = ( { moviesArray } ) => {
                         </MovieItemElementDiv>
                     </MovieItemTopDiv>
                         <MovieItemDetailsDiv>
+
+                            <MovieItemDetailsPoster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.original_title} Poster`} /> 
                             <MovieItemPlotDiv>
+                                <StarRating stars={movie.vote_average/2}/>
                                 {movie.overview}
                             </MovieItemPlotDiv>
                         </MovieItemDetailsDiv>
@@ -83,8 +101,11 @@ export const CollapsibleMovie = ( { moviesArray } ) => {
                 {/* setWatchlist([ ...watchlist, movie.title]) */}
                 <MovieSearchDiv>
                 {/* MovieSearchDiv */}
-                <MovieSearchInput onClick={(e) => SearchMovie(e.target.value)} />
-            </MovieSearchDiv>
+                {/* <MovieSearchInput onClick={(e) => SearchMovie(e.target.value)} /> */}
+                    <MovieSearchInput onChange={(e) => setMovieSearch(e.target.value)} />
+                    <button onClick={() => SearchMovie(movieSearch)}>Search</button>
+
+                </MovieSearchDiv>
                 { movieResults && movieResults.map(
                         (item, index) => <MovieItem movie = {item} />
                     )
@@ -103,7 +124,7 @@ const MovieSearchDiv = styled.div`
     display: flex;
     border: black 4px solid;
     margin: 2px;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
     height: 3em;
@@ -111,13 +132,19 @@ const MovieSearchDiv = styled.div`
 
 `
 const MovieSearchInput = styled.input`
-    width: 60%;
+    width: 45%;
     height: 2em;
+    margin: 1em;
 `
 
 const Logo = styled.img`
     height: 100%
     width: 100%
+`
+
+const SmallLogo = styled.img`
+    height: 1em;
+    width: 1em;
 `
 // const MovieListDiv = styled.div`
 //     border: black 4px solid;
@@ -146,6 +173,11 @@ const MovieItemTopDiv = styled.div`
     width: 24em;
 `
 
+const StarDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
 const MovieItemElementDiv = styled.div`
     // display: flex;
     // justify-content: stretch;
@@ -157,12 +189,18 @@ const MovieItemElementDiv = styled.div`
     // background-color: red;
 `
 const MovieItemDetailsDiv = styled.div`
+    display: flex;
+    flex-direction: row;
     border: black 4px solid;
     margin: 2px;
     flex: 1;
     width: 24em;
     // height: 2em;
     // width: 2em;
+`
+
+const MovieItemDetailsPoster = styled.img`
+    width: 30%;
 `
 const MovieItemPlotDiv = styled.div`
     border: black 4px solid;
