55514f4d381c077b9963ca1da5850779bd497a7a
ARW <ARWayland@gmail.com>
2022-03-25 10:37:39 +0000

    stashed due to conflicts

--- src/pages/userSettings.jsx
+++ src/pages/userSettings.jsx
@@ -4,19 +4,24 @@ import { Footer } from "../components/footer";
 import { updatePass, updateEmail } from "../utils";
 import Navbar from "../components/navbar";
 
-// export const UserSettings = ({ user, setUser }) => {
-//     //Changes document title on load
-//     useEffect(() => {
-//         document.title = "Account / ViewFinder";
-//     }, []);
+export const UserSettings = ({ user, setUser }) => {
+    const []
+    
+    //Changes document title on load
+    useEffect(() => {
+        document.title = "Account / ViewFinder";
+    }, []);
 
-//     //useEffect for getting user data here?
-//     useEffect(() => {}, []);
+    //useEffect for getting user data here?
+    // useEffect(() => {}, []);
 
     //Function to change email? Stretch goal
     // const emailChanger = (user, emailUpdate) => {};
     //Function to change password
-const passwordChanger = (user, passUpdate) => {
+    const submitHandler = (e) => {
+        e.preventDefault();
+        updatePass(user, passUpdate);
+    };
 
     return (
         <>
@@ -25,17 +30,20 @@ const passwordChanger = (user, passUpdate) => {
                 <h2>Welcome, {user}</h2>
                 <p>Would you like to change your password or email address?</p>
             </div>
-            <form id="settings-change-password" onSubmit={(e) => {}}>
-                <input type="password" placeholder="password" />
+            <form id="settings-change-password" onSubmit={submitHandler}>
+                <input
+                    type="password"
+                    placeholder="password"
+                    onChange={(e) => setPassUpdate(e.target.value)}
+                />
             </form>
             <Footer />
         </>
     );
-
 };
 
-// {
-//     /* <form id="settings-change-email" onSubmit={(e) => {}}>
-// <input type="email" placeholder={userEmail} />
-// </form> */
-// }
+{
+    /* <form id="settings-change-email" onSubmit={(e) => {}}>
+<input type="email" placeholder={userEmail} />
+</form> */
+}
